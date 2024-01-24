9a44639f96ef3f545057e9f26d081ef878ef4f42
qudehu <qudehu@163.com>
2024-01-24 20:56:11 +0800

    代码生成器的使用

---
+++ qudehu-generator-web/README.md
@@ -0,0 +1,6 @@
+启动
+>/test/GeberatorUIServer.java 
+> 
+[浏览器访问](http://localhost:8068) http://localhost:8068
+
+即可进行代码生成
---
+++ qudehu-generator-web/src/main/java/com/dehu/generatorweb/QudehuGeneratorWebApplication.java
@@ -0,0 +1,13 @@
+package com.dehu.generatorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QudehuGeneratorWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QudehuGeneratorWebApplication.class, args);
+    }
+
+}
